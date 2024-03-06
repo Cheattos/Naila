5724f36c8e052c24ef3c2bb5ebdc04725412db47
118140129Arre <80655807+118140129Arre@users.noreply.github.com>
2024-03-06 17:24:11 +0700

    first

---
+++ index.js
@@ -0,0 +1,49 @@
+import express from 'express';
+import hbs from 'hbs';
+
+const app = express();
+const port = 3000;
+
+app.set("view engine", "hbs");
+app.set("views", "src/views");
+
+app.use('/assets', express.static('src/assets'));
+app.use(express.urlencoded({ extended: false }));
+
+const datas = [];
+app.get('/', home);
+app.get('/myproject', myproject);
+app.get('/testimonial', testimony);
+app.get('/contact', contact_me);
+app.get('/project-detail/:id', detail_project);
+app.post('/myproject', handlePostProject);
+app.get('/delete-project/:id', handleDeleteProject);
+
+function home(req, res) {
+  res.render('home');
+}
+function myproject(req, res) {
+  res.render('index');
+}
+function testimony(req, res) {
+  res.render('index');
+}
+function contact_me(req, res) {
+  res.render('index');
+}
+function detail_project(req, res) {
+  res.render('index');
+}
+function handlePostProject(req, res) {
+  datas.push(req.body);
+  res.redirect('/myproject');
+}
+
+function handleDeleteProject(req, res) {
+  const projectId = req.params.id;
+  res.redirect('/myproject');
+}
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
